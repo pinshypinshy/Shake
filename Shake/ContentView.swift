f7bcd3d561318c7ec30d0ddcf4d4844f47efaf4f
pinshypinshy <hrisyklit@gmail.com>
2026-02-11 11:13:30 +0900

    Initial Commit

---
+++ Shake/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shake
+//
+//  Created by 平石悠生 on 2026/02/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shake/ShakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShakeApp.swift
+//  Shake
+//
+//  Created by 平石悠生 on 2026/02/11.
+//
+
+import SwiftUI
+
+@main
+struct ShakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
